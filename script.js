251e8b2dc1316dbf9484220fd2b0916342a0c961
kimdonghyeon-97 <78817995+kimdonghyeon-97@users.noreply.github.com>
2022-02-02 20:26:15 +0900

    add script.js comment
    that's it

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const tagsEl = document.getElementById('tags')
 const textarea = document.getElementById('textarea')
 
-textarea.focus()
+textarea.focus() // 화면 진입시 textarea에 입력준비가 되어짐
 
-textarea.addEventListener('keyup', (e) => {
+textarea.addEventListener('keyup', (e) => { // keyup : 누른 키에서 손 떼면 실행
     createTags(e.target.value)
 
     if(e.key === 'Enter') {
@@ -50,13 +50,18 @@ function randomSelect() {
 
             highlightTag(randomTag)
         }, 100)
-    }, times * 100)
-}
+    }, times * 100) // 그냥 times만 놓으면 매우 느림
+} 
 
 function pickRandomTag() {
     const tags = document.querySelectorAll('.tag')
     return tags[Math.floor(Math.random() * tags.length)]
-}
+} // Math.random() 은 0~1 까지의 무작위 숫자를 반환하고
+  // Math.floor()은 주어진 숫자와 같거나 작은 정수를 반환한다.
+  // Ex : Math.floor(1.2) = 1
+// 따라서 tags.length가 3이라고 할 때, pickRandomTag()의 반환 값은 0 ~ 2다.
+// 그리하여 Random한 Tag를 반환하여 나중에 highlight할 때 사용할 수 있는 것이다.
+
 
 function highlightTag(tag) {
     tag.classList.add('highlight')
